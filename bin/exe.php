c8a55ea2d9578a7538c8fac1a404acc0d1365192
Alexander Hutorezki <alexander.hutorezki@skon.de>
2019-01-18 04:45:58 +0100

    Added game runner

--- bin/exe.php
+++ bin/exe.php
@@ -11,14 +11,14 @@ use Olekhy\Spiel\RandomSymbolTossStrategy;
 use Olekhy\Spiel\Reporter;
 use Olekhy\Spiel\StoneScissorPaperRules;
 
-$bobStrategy = new FixStoneTossStrategy();
-$bob = new Gambler($bobStrategy, 'Bob');
-$aliceStrategy = new  RandomSymbolTossStrategy();
-$alice = new Gambler($aliceStrategy, 'Alice');
+$bobStrategy   = new FixStoneTossStrategy();
+$bob           = new Gambler($bobStrategy, 'Bob');
+$aliceStrategy = new RandomSymbolTossStrategy();
+$alice         = new Gambler($aliceStrategy, 'Alice');
 
 $reporter = new Reporter();
-$rules = new StoneScissorPaperRules();
-$game = new Game($bob, $alice, $reporter);
-$result = $game->play(100, $rules);
+$rules    = new StoneScissorPaperRules();
+$game     = new Game($bob, $alice, $reporter);
+$result   = $game->play(100, $rules);
 
 $result->print();
